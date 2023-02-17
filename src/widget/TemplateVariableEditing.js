d083efd8d79de88872c8d7bacdbe0096cc11adb2
toandangcong <toan.dang-cong@banvien.com.vn>
2023-02-17 08:43:04 +0700

    upgrade version

--- src/widget/TemplateVariableEditing.js
+++ src/widget/TemplateVariableEditing.js
@@ -1,14 +1,14 @@
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import {
 	toWidget,
-	viewToModelPositionOutsideModelElement,
-} from "@ckeditor/ckeditor5-widget/src/utils";
-import Widget from "@ckeditor/ckeditor5-widget/src/widget";
-import TemplateVariableCommand from "./TemplateVariableCommand";
+	viewToModelPositionOutsideModelElement
+} from '@ckeditor/ckeditor5-widget/src/utils';
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
+import TemplateVariableCommand from './TemplateVariableCommand';
 
 export default class ProductPreviewEditing extends Plugin {
 	static get requires() {
-		return [Widget];
+		return [ Widget ];
 	}
 
 	init() {
@@ -16,15 +16,15 @@ export default class ProductPreviewEditing extends Plugin {
 		this._defineConverters();
 
 		this.editor.commands.add(
-			"insertTemplateVariable",
-			new TemplateVariableCommand(this.editor)
+			'insertTemplateVariable',
+			new TemplateVariableCommand( this.editor )
 		);
 
 		this.editor.editing.mapper.on(
-			"viewToModelPosition",
+			'viewToModelPosition',
 			viewToModelPositionOutsideModelElement(
 				this.editor.model,
-				(viewElement) => viewElement.hasClass("template-variable")
+				viewElement => viewElement.hasClass( 'template-variable' )
 			)
 		);
 	}
@@ -32,11 +32,11 @@ export default class ProductPreviewEditing extends Plugin {
 	_defineSchema() {
 		const schema = this.editor.model.schema;
 
-		schema.register("templateVariable", {
-			inheritAllFrom: "$inlineObject",
-			allowAttributes: ["name"],
-			allowWhere: "$text",
-		});
+		schema.register( 'templateVariable', {
+			inheritAllFrom: '$inlineObject',
+			allowAttributes: [ 'name' ],
+			allowWhere: '$text'
+		} );
 	}
 
 	_defineConverters() {
@@ -44,57 +44,57 @@ export default class ProductPreviewEditing extends Plugin {
 		const conversion = editor.conversion;
 
 		// <productPreview> converters ((data) view → model)
-		conversion.for("upcast").elementToElement({
+		conversion.for( 'upcast' ).elementToElement( {
 			view: {
-				name: "span",
-				classes: ["template-variable"],
+				name: 'span',
+				classes: [ 'template-variable' ]
 			},
-			model: (viewElement, modelWriter) => {
-				const name = viewElement.getChild(0).data;
+			model: ( viewElement, modelWriter ) => {
+				const name = viewElement.getChild( 0 ).data;
 
-				return modelWriter.createElement("templateVariable", {
-					name,
-				});
-			},
-		});
+				return modelWriter.createElement( 'templateVariable', {
+					name
+				} );
+			}
+		} );
 
 		// <templateVariable> converters (model → data view)
-		conversion.for("dataDowncast").elementToElement({
-			model: "templateVariable",
-			view: (modelElement, viewWriter) => {
-				return createTemplateVariableView(modelElement, viewWriter);
-			},
-		});
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'templateVariable',
+			view: ( modelElement, viewWriter ) => {
+				return createTemplateVariableView( modelElement, viewWriter );
+			}
+		} );
 
 		// <templateVariable> converters (model → editing view)
-		conversion.for("editingDowncast").elementToElement({
-			model: "templateVariable",
-			view: (modelElement, viewWriter) => {
+		conversion.for( 'editingDowncast' ).elementToElement( {
+			model: 'templateVariable',
+			view: ( modelElement, viewWriter ) => {
 				const widgetElement = createTemplateVariableView(
 					modelElement,
 					viewWriter
 				);
 
 				// Enable widget handling on a placeholder element inside the editing view.
-				return toWidget(widgetElement, viewWriter);
-			},
-		});
+				return toWidget( widgetElement, viewWriter );
+			}
+		} );
 
 		// Helper method for both downcast converters.
-		function createTemplateVariableView(modelElement, viewWriter) {
-			const name = modelElement.getAttribute("name");
+		function createTemplateVariableView( modelElement, viewWriter ) {
+			const name = modelElement.getAttribute( 'name' );
 
 			const templateVariableView = viewWriter.createContainerElement(
-				"span",
+				'span',
 				{
-					class: "template-variable",
+					class: 'template-variable'
 				}
 			);
 
 			// Insert the placeholder name (as a text).
-			const innerText = viewWriter.createText(name);
+			const innerText = viewWriter.createText( name );
 			viewWriter.insert(
-				viewWriter.createPositionAt(templateVariableView, 0),
+				viewWriter.createPositionAt( templateVariableView, 0 ),
 				innerText
 			);
 
